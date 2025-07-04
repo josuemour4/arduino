2b12477245b66eb7a04b1821968db976b2f6205e
Josue <154309452+josuemour4@users.noreply.github.com>
2025-07-05 07:22:09 +1000

    Update readButton.ino

--- readButton.ino
+++ readButton.ino
@@ -8,9 +8,12 @@
 /*
 This is another example of a simple project from the Arduino Basics book that I am studying.
 
-The goal is to detect a signal change on a digital pin (D7), which simulates, for example, a light barrier sensor. When the barrier is interrupted — such as when someone passes through it — the input value changes from 0 to 1 (depending on the sensor).
+The goal is to detect a signal change on a digital pin (D7), which simulates, for example, 
+a light barrier sensor. When the barrier is interrupted — such as when someone passes through it — 
+the input value changes from 0 to 1 (depending on the sensor).
 
-The code reads this value and replicates it on pin D9, which can be connected to a warning light or a machine stop system.
+The code reads this value and replicates it on pin D9, which can be connected to a warning light or
+a machine stop system.
 
 I also added Serial communication to view the state change (0 or 1) on the serial monitor.
 
